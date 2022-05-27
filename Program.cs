dc4c9baaaaefbaf46d9533087ab3292200649465
Lander Van Beeck <landervanbeeck@hotmail.com>
2022-05-27 10:21:26 +0200

    Coding challenge

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int i, n;
+double s = 0.0;
+
+Console.WriteLine("input: ");
+n = Convert.ToInt32(Console.ReadLine());
+for ( i = 1; i <= n; i++)
+{
+    Console.Write("1/{0} + ", i);
+    s += 1 / (float)i;
+}
+Console.Write("\ninput {0} returns output {1} \n", n, s.ToString("0.0000"));
